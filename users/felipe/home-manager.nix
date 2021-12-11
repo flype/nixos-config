c9c988d0838183a8cc3d887e5b60ea4eed6828a3
Felipe Talavera <felipe.talavera@gmail.com>
2021-12-11 21:26:28 +0100

    removes the whitelists for now

--- users/felipe/home-manager.nix
+++ users/felipe/home-manager.nix
@@ -68,20 +68,7 @@ let sources = import ../../nix/sources.nix; in {
     };
   };
 
-  programs.direnv= {
-    enable = true;
-
-    config = {
-      whitelist = {
-        prefix= [
-          "$HOME/code/go/src/github.com/hashicorp"
-          "$HOME/code/go/src/github.com/mitchellh"
-        ];
-
-        exact = ["$HOME/.envrc"];
-      };
-    };
-  };
+  programs.direnv.enable = true;
 
   programs.fish = {
     enable = true;
