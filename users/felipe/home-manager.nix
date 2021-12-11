f5bd4b75937e20533a3c95c6555e4c2f2f5badb1
Felipe Talavera <felipe.talavera@gmail.com>
2021-12-11 20:55:19 +0100

    add suport back to direnv

--- users/felipe/home-manager.nix
+++ users/felipe/home-manager.nix
@@ -68,6 +68,20 @@ let sources = import ../../nix/sources.nix; in {
     };
   };
 
+  programs.direnv= {
+    enable = true;
+
+    config = {
+      whitelist = {
+        prefix= [
+          "$HOME/code/go/src/github.com/hashicorp"
+          "$HOME/code/go/src/github.com/mitchellh"
+        ];
+
+        exact = ["$HOME/.envrc"];
+      };
+    };
+  };
 
   programs.fish = {
     enable = true;
