57e472727071fa775ba0fe639b76883cb34c280b
Felipe Talavera <felipe@talavera.dev>
2022-05-25 12:48:32 +0200

    adding postman to the machine

--- users/felipe/home-manager.nix
+++ users/felipe/home-manager.nix
@@ -32,6 +32,7 @@ let sources = import ../../nix/sources.nix; in {
     pkgs.vscode
     pkgs.ripgrep
     pkgs.cloudflared
+    pkgs.postman
   ];
 
   #---------------------------------------------------------------------
